406269a9ffcdc06061de824b0cac78ec46f2b71c
hankotanks <hank@modernatomic.com>
2022-02-24 10:03:31 -0700

    Updated build.sh to accommodate new build file locations

--- build.sh
+++ build.sh
@@ -1,2 +1,17 @@
+files=()
+for file in "$PWD/build"/*; do
+  if [ -f "$file" ]; then
+    files+="${file##*/}"
+  fi
+done
+cp -R $PWD/build/. $PWD/
+
 python ./setup.py develop
+
+for file in "$PWD"/*; do
+  if [[ "${files[*]}" =~ ${file##*/} ]]; then
+    rm $file
+  fi
+done
+
 read -n 1 -s
