def581255d0722c9114d135084c2ccdcb46f1be2
hankotanks <hank@modernatomic.com>
2022-02-24 10:11:22 -0700

    Commented build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,23 @@
+# create list of build filenames for matching later
 files=()
 for file in "$PWD/build"/*; do
   if [ -f "$file" ]; then
     files+="${file##*/}"
   fi
 done
-cp -R $PWD/build/. $PWD/
 
+# copy build files to project root
+cp -R "$PWD"/build/. "$PWD"/
+
+# run build
 python ./setup.py develop
 
+# remove all build files from project root
 for file in "$PWD"/*; do
   if [[ "${files[*]}" =~ ${file##*/} ]]; then
-    rm $file
+    rm "$file"
   fi
 done
 
+# wait for user input to close
 read -n 1 -s
