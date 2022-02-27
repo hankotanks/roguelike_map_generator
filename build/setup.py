b10fae8b06abd430016fe423092c7b6760a660a8
hankotanks <hank@modernatomic.com>
2022-02-27 07:58:55 -0700

    Removed submodule to simplify the required import

--- build/setup.py
+++ build/setup.py
@@ -4,6 +4,6 @@ from setuptools_rust import Binding, RustExtension
 setup(
     name="map_generator",
     version="1.0",
-    rust_extensions=[RustExtension("map_generator.map_generator", binding=Binding.PyO3)],
+    rust_extensions=[RustExtension("map_generator", binding=Binding.PyO3)],
     zip_safe=False
 )
