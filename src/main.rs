234348a1f9918abda11195294f8f2d02f1033b2a
hankotanks <hank@modernatomic.com>
2022-03-14 16:22:32 -0600

    Added a test file called main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use map_generator::build;
+use colored::*;
 
 use std::collections::HashMap;
+use std::time::Instant;
 
 fn main() {
 
-    let symbols: HashMap<u8, char> = HashMap::from([
-        (0, ' '),
-        (1, '#'),
-        (2, '%')
+    let symbols: HashMap<u8, ColoredString> = HashMap::from([
+        (0, " ".white().on_black()),
+        (1, "#".black().on_blue()),
+        (2, "%".black().on_magenta()),
+        (3, "+".magenta().on_black())
     ]);
-
+    let now = Instant::now();
     let world = build(32, 98, None);
+    let elapsed = now.elapsed();
 
     for y in 0..world.len() {
         for x in 0..world[0].len() {
@@ -18,4 +22,6 @@ fn main() {
         }
         println!();
     }
+
+    println!("{:.2?}", elapsed);
 }
